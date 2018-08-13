4db8094e4bd5e30a4ce07ce2cc1654e985896ff9
caoqiang666 <952631256@qq.com>
2018-08-13 23:44:43 +0800

    试验了restTemplate

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -6,8 +6,10 @@ import com.example.demo.Service.commen.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -19,14 +21,21 @@ public class HelloController {
     @Resource(name="userService")
     private UserService userService;
     @Autowired
-    private RedisService redisService;
-    @Autowired
     private ValueOperations valueOperations;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/hello")
     public String sayHello(){
         valueOperations.increment("count",1);
         return  "Hello World!"+valueOperations.get("count");
+    }
+
+    @RequestMapping("/testRest")
+    public String testRest(){
+        ResponseEntity<String> responseEntity=restTemplate.getForEntity("https://mini.eastday.com/json/index/indexMergeNews.json?callback=indexMergeNews&_=1534171368541",String.class);
+        String reutrnStr =  restTemplate.getForObject("https://mini.eastday.com/json/index/indexMergeNews.json?callback=indexMergeNews&_=1534171368541",String.class);
+        return reutrnStr;
     }
 
     @RequestMapping("/getUser")
